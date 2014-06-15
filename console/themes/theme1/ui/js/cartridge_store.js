e880812e02f7bde85830e6d6035632efa23304fa
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2014-06-16 05:28:26 +0530

    latest update on store

--- console/themes/theme1/ui/js/cartridge_store.js
+++ console/themes/theme1/ui/js/cartridge_store.js
@@ -1,13 +1,20 @@
 var ajaxerror_handler='';
+var cartridge_set;
+var puppet_data;
 var assets = '';
-var sortassets;
+var asset='';
+var sortassets={};
 var offsetcount=8;
 var offset= 8;
 var start = 0;
+var sortOrder = "DES";
+var sortBy = "overview_createdtime";
+var search_word=''; 
 //ajax erorr handler
 $(function() {
 	$.ajaxSetup({
 		error: function(jqXHR, exception) {
+			console.log(exception);
 			if (jqXHR.status === 0) {
 				ajaxerror_handler = 'Not connect.\n Verify Network.';
 			} else if (jqXHR.status == 404) {
@@ -43,7 +50,7 @@ $(document).ready(function(){
 	   	setTimeout(function(){
 	   		start = offset;
 	   		offset = offset+8;
-	   		getassets(start, offset);
+	   		getassets(start, offset, sortOrder, sortBy);
 	   	}, 500);
 	    	
 	   }
@@ -52,27 +59,43 @@ $(document).ready(function(){
 
 });
 
-function getassets(start, offset){
+function getassets(start, offset, sortOrder, sortBy, search){
 	//init base ajax call to get initial set
 		//add loading effect 
-	var ustart= start || 0;
-	var ucount= offset || 8;
+	var ustart= start || 0,
+		ucount= offset || 8,
+		usortOrder= sortOrder || "DES",
+		usearch= search || '',
+		usortBy = sortBy || "overview_createdtime";
 
 	$('.loader').html("<img src='themes/theme1/ui/img/loading.gif' />");
 	//disable search button to prevent 
-	$('#searchbtn').attr('disabled','disabled');
+	//$('#searchbtn').attr('disabled','disabled');
 	//console.log(Object.keys(jQuery.parseJSON(assets)).length);
 	if(assets != '' && Object.keys(assets).length < offsetcount){
-		$('.scroll-more').html('No More cartridge available');
-		$('.scroll-more').removeClass( "alert-info" ).addClass( "alert-danger" )
+		$('.scroll-more').html('No More cartridges available');
+		$('.scroll-more').removeClass( "alert-info" ).addClass( "alert-danger" );
 		$('.loader').html('');
 	}else{
+
+
+
+		//get json from store
 		$.ajax({
-		  url: "cartridge_store/cartridgestore_search.jag?start="+ustart+"&count="+ucount,
+		  url: "cartridge_store/cartridgestore_search.jag?start="+ustart+"&count="+ucount+"&sortOrder="+usortOrder+"&sortBy="+usortBy+"&search="+usearch,
 		}).done(function(result) {
-		    assets = $.parseJSON(result);
-		    sortassets = assets; 
-		    $('.cartridge-load').append(assetHTML(assets));
+		   	cartridge_set = $.parseJSON(result);
+		   	puppet_data = cartridge_set.puppet_data;
+
+//console.log(puppet_data);
+		    assets = cartridge_set.store_data;
+		     if(Object.keys(sortassets).length >0 ){
+		     	sortassets = $.merge(sortassets,assets)
+		     }else{
+		     	sortassets =  assets; 
+		     }
+		    
+		    $('.cartridge-load').append(assetListHTML(assets, puppet_data));
 		    $('.loader').html('');
 		});
 	}
@@ -80,13 +103,28 @@ function getassets(start, offset){
 
 }
 
-//generate assets
-function assetHTML(assets){
-	var html='';
+//generate assets list
+function assetListHTML(assets, puppet_data){
+	var html='', button_text='', filename ='', button_style='';
 	//return html base on that
 	if(assets){
-
+		console.log(assets + 'adooooo ');
 	$.each(assets, function(idx, obj) {
+
+		filename = (obj.attributes.overview_url.substring(obj.attributes.overview_url.lastIndexOf('/')+1)).split(".")[0];
+		
+		if ($.inArray( filename , puppet_data.installed) > 0) {
+          	button_text = 'Deploy';
+          	button_style = 'btn-danger asset-done-btn';
+        }else if($.inArray( filename , puppet_data.inprogress) > 0){
+        	button_text = 'Progressing';
+          	button_style = 'btn-default asset-done-btn';
+        }
+        else{
+        	button_text = 'Install';
+        	button_style = 'btn-primary asset-download-btn';
+        }
+		
 		html +='<div class="span3 asset">';
 		html += 	'<a href="" id="'+obj.id+'" class="asset-img-hover">';
 		html += 	'<div class="asset-icon">';
@@ -99,13 +137,63 @@ function assetHTML(assets){
 		html +=				'<li><h4>Version</h4><a class="asset-version" href="#">'+obj.attributes.overview_version+'</a></li>';
 		//html +=				'<li><h4>Category</h4><a class="asset-category" href="#">'+obj.type+'</a></li>';
 		html +=				'<li><h4>Author</h4><a class="asset-author" href="#">'+obj.attributes.overview_provider+'</a></li>';
-		html +=				'<li><button type="button" class="btn btn-default asset-detail-btn" > Details </button></li>';
-		html +=				'<li><button type="button" class="btn btn-primary asset-download-btn" > Install </button></li>';
+		html +=				'<li><button type="button" class="btn btn-default asset-detail-btn" id="'+obj.id+'"> Details </button></li>';
+		html +=				'<li><button type="button" class="btn '+button_style+' " id="'+obj.id+'">'+button_text+'</button></li>';
 		html +=			'</ul></div>';
 		html +=		'</div></div>';
+		html += '';
 	});
 	
 
+	}
+
+	if(html==''){
+		$('.scroll-more').html('No Cartridge found');
+		$('#searchtxt').val('');
+		start = 0;
+		offset =8;
+		assets ='';
+	}
+	
+
+	return html;
+}
+
+//generate single asset details view
+function assetHTML(asset, puppet_data){
+	var html='';
+	if (asset) {
+		html += '<div class="asset-description">';
+        html += '	<div class="asset-description-header">';
+        html += '   	<div class="row">';
+        html += '            <div class="span9">';
+        html += '                <div class="asset-banner">';
+        html += '                   <img src="'+asset.attributes.images_banner+'">';
+        html += '               </div>';
+        html += '               <div class="asset-introduction-box">';
+        html += '                   <img src="'+asset.attributes.images_thumbnail+'">';
+        html += '                   <h3>'+asset.attributes.overview_name+'</h3>';
+        html += '                   <small> Version : '+asset.attributes.overview_version+'</small>';
+        html += '                   <small> Version : '+asset.attributes.overview_category+'</small>';
+        html += '                   <small>  by :  '+asset.attributes.overview_provider+' </small>';	
+		html +=				'<button type="button" class="btn btn-primary asset-download-detail-btn" id="'+asset.id+'"> Install </button>';
+        html += '                </div>';
+		html += '			</div>';
+		html += '		</div>';
+        html += '    </div>';
+        html += '<div class="assetp-properties">';
+        html += '<ul class="nav nav-tabs">';
+        html += '	<li class="active"><a href="#tab-properties" data-toggle="tab" data-type="basic">Description</a></li>';
+        html += '        </ul>';
+        html += '        <div class="tab-content">';
+        html += '            <div class="tab-pane active" id="tab-properties">';
+        html += '                <div class="content">';
+		html += asset.attributes.overview_description;
+        html += '                </div>';
+        html += '            </div>';
+        html += '        </div>';
+        html += '</div>';
+        html += '</div>';
 	}
 
 	return html;
@@ -123,54 +211,137 @@ $(document).bind("DOMSubtreeModified",function(){
 });
 
 
+//to trigger asset details
 $(document).on('click', '.asset-detail-btn', function () {
     // click handler code goes here
-    console.log('hari do');
     $('#lightbox').fadeIn('slow');
+    var cid = $(this).attr('id');
+    $.ajax({
+		  url: "cartridge_store/cartridgestore_asset.jag?cid="+cid,
+		}).done(function(result) {
+		    asset = $.parseJSON(result);
+		    $('#asset-detail-box').html(assetHTML(asset));
+		});
 });
 
 
+//close down popup asset details
 $('.icons-circle-close').click(function(){
 	$('#lightbox').fadeOut('slow');
 });
 
 
-$('#searchbtn').click(function(){
+
+//handling sorting functions
+
+$('.sortbyname .atoz').click(function(){
+	$('.sortbyname .atoz').css('display', 'none');
+	$('.sortbyname .ztoa').css('display', 'block');
+	$('.cartridge-load').html('');
+	start = 0;
+	offset =8;
+	assets ='';
+	sortOrder = "DES";
+	sortBy = "overview_name";
+	search_word = $('#searchtxt').val();
+	$('.scroll-more').html('Scroll Down to load more...');
+	$('.scroll-more').removeClass( "alert-danger" ).addClass( "alert-info" );
+	getassets(start, offset, sortOrder, sortBy, search_word);
+});
+
+$('.sortbyname .ztoa').click(function(){
+	$('.sortbyname .ztoa').css('display', 'none');
+	$('.sortbyname .atoz').css('display', 'block');
+	$('.cartridge-load').html('');
+	start = 0;
+	offset =8;
+	assets ='';
+	sortOrder = "ASC";
+	sortBy = "overview_name";
+	search_word = $('#searchtxt').val();
+	$('.scroll-more').html('Scroll Down to load more...');
+	$('.scroll-more').removeClass( "alert-danger" ).addClass( "alert-info" );
+	getassets(start, offset, sortOrder, sortBy, search_word);
+});
+
+
+$('.sortbydate .atoz').click(function(){
+	$('.sortbydate .atoz').css('display', 'none');
+	$('.sortbydate .ztoa').css('display', 'block');
+	$('.cartridge-load').html('');
+	start = 0;
+	offset =8;
+	assets ='';
+	sortOrder = "DES";
+	sortBy = "overview_createdtime";
+	search_word = $('#searchtxt').val();
+	$('.scroll-more').html('Scroll Down to load more...');
+	$('.scroll-more').removeClass( "alert-danger" ).addClass( "alert-info" );
+	getassets(start, offset, sortOrder, sortBy, search_word);
+});
+
+$('.sortbydate .ztoa').click(function(){
+	$('.sortbydate .ztoa').css('display', 'none');
+	$('.sortbydate .atoz').css('display', 'block');
+	$('.cartridge-load').html('');
+	start = 0;
+	offset =8;
+	assets ='';
+	sortOrder = "ASC";
+	sortBy = "overview_createdtime";
+	search_word = $('#searchtxt').val();
+	$('.scroll-more').html('Scroll Down to load more...');
+	$('.scroll-more').removeClass( "alert-danger" ).addClass( "alert-info" );
+	getassets(start, offset, sortOrder, sortBy, search_word);
+});
+
+//search function
+function searchcartridge(search_word){
+	var resultfind;
+	var notification ='';
 	
-	searchcartridge(assets);
+	if(search_word ==''){
+		$('.cartridge-load').html('');
+		$('.scroll-more').html('Please Enter Cartridge Name on Search field');
+		$('.scroll-more').removeClass( "alert-danger" ).addClass( "alert-warning" );	
+	}else{
+		start = 0;
+		offset =8;
+		assets ='';
+		$('.cartridge-load').html('');
+		$('.scroll-more').html('Scroll Down to load more...');
+		$('.scroll-more').removeClass( "alert-danger" ).addClass( "alert-info" );
+		getassets(start, offset, sortOrder, sortBy,search_word);
+	}
+
+
+}
+
+$('#searchbtn').click(function(){
+	var search_word = $('#searchtxt').val();
+	searchcartridge(search_word);
 });
 
 $(document).keypress(function(e) {
     if(e.which == 13) {
-        searchcartridge(assets);
+        var search_word = $('#searchtxt').val();
+		searchcartridge(search_word);
     }
 });
 
+//to trigger install button
+$(document).on('click', '.asset-download-btn', function () {
+    // click handler code goes here
+   // $('#lightbox').fadeIn('slow');
 
+    var cid = $(this).attr('id');
+    $(this).html('Progressing');
+    $(this).removeClass('btn-primary').addClass('btn-default');
+    $(this).attr('disabled','disabled');
 
-
-
-//handling sorting functions
-function SortByName(a, b){
-  var aName = a.attributes.overview_name.toLowerCase();
-  var bName = b.attributes.overview_name.toLowerCase(); 
-  return ((aName < bName) ? -1 : ((aName > bName) ? 1 : 0));
-}
-
-$('.atoz').click(function(){
-	$('.atoz').css('display', 'none');
-	$('.ztoa').css('display', 'block');
-	
-	var assets3 = sortassets;
-	assets3.sort(SortByName).reverse();
-	$('.cartridge-load').html( assetHTML(assets3));
-});
-
-$('.ztoa').click(function(){
-	$('.ztoa').css('display', 'none');
-	$('.atoz').css('display', 'block');
-	
-	var assets3 = sortassets;
-	assets3.sort(SortByName);
-	$('.cartridge-load').html( assetHTML(assets3));
+    $.ajax({
+		  url: "cartridge_store/cartridgestore_install.jag?cid="+cid,
+		}).done(function(result) {
+		   console.log(result);
+		});
 });
